3e1a1389388956f34e3e9dcb9ef1190e31f2082f
umanghalok <umanghalok@gmail.com>
2024-08-28 07:46:48 +0530

    minor bugs

--- routes/answer.js
+++ routes/answer.js
@@ -3,15 +3,14 @@ import {addAnswer,fetchAnswer,fetchOneAnswer,fetchAnswerById,updateAnswer,fetchO
     fetchUserFilteredAnswers,fetchAllFilteredAnswers,givenAllAnswersTags,givenAnswersTags,fetchUserAnsweredQuestions,
     findNumberOfAnswer,upvote,downvote,fetchVotes,deleteAnswer
 } from '../controllers/answer.controller.js';
-import { authenticateToken } from '../middleware/jwt.middleware.js';
 
 const router= Router();
 
-router.route('/addanswer/:id').post(authenticateToken,addAnswer);
+router.route('/addanswer/:id').post(addAnswer);
 router.route('/fetchanswer').get(fetchAnswer);
 router.route('/fetchanswer/:id').get(fetchOneAnswer);
 router.route('/fetchAnswerById/:id').get(fetchAnswerById);
-router.route('/updateanswer/:id').put(authenticateToken,updateAnswer);
+router.route('/updateanswer/:id').put(updateAnswer);
 router.route('/fetchUserAnswers/:username').get(fetchOneUserAnswer);
 router.route('/fetchUserFilteredAnswers').get(fetchUserFilteredAnswers);
 router.route('/fetchAllFilteredAnswers').get(fetchAllFilteredAnswers);
@@ -19,8 +18,8 @@ router.route('/givenAllAnswersTags').get(givenAllAnswersTags);
 router.route('/givenAnswersTags/:username').get(givenAnswersTags);
 router.route('/fetchUserAnsweredQuestions').get(fetchUserAnsweredQuestions);
 router.route('/findNumberOfAnswer').get(findNumberOfAnswer);
-router.route('/upvote/:id').put(authenticateToken,upvote);
+router.route('/upvote/:id').put(upvote);
 router.route('/fetchVotes').get(fetchVotes);
-router.route('/downvote/:id').put(authenticateToken,downvote);
+router.route('/downvote/:id').put(downvote);
 router.route('/deleteanswer/:id').delete(deleteAnswer);
 export default router;
--- routes/question.js
+++ routes/question.js
@@ -1,22 +1,22 @@
 import {Router} from 'express';
 import {questionTags,questionByMonth,addQuestion,fetchQuestions,fetchAllVotes,fetchFilteredQuestions,fetchQuestionsByHigherVotes,fetchQuestionsById,fetchUserFilteredQuestions,fetchUserQuestions,fetchVotes,updateQuestion,upvote,usedTags,unansweredQuestions,usedUserTags,deleteQuestion,downvote,answeredQuestions} from '../controllers/question.controller.js';
-import { authenticateToken } from '../middleware/jwt.middleware.js';
+
 
 const router= Router();
 
-router.route('/addquestion').post(authenticateToken,addQuestion);
+router.route('/addquestion').post(addQuestion);
 router.route('/fetchquestions').get(fetchQuestions);
 router.route('/fetchQuestionsByHigherVotes').get(fetchQuestionsByHigherVotes);
 router.route('/fetchQuestionsById/:id').get(fetchQuestionsById);
-router.route('/updatequestion/:id').put(authenticateToken,updateQuestion);
+router.route('/updatequestion/:id').put(updateQuestion);
 router.route('/deletequestion/:id').delete(deleteQuestion);
 router.route('/fetchFilteredQuestions').get(fetchFilteredQuestions);
 router.route('/fetchUserQuestions').get(fetchUserQuestions);
 router.route('/fetchUserFilteredQuestions').get(fetchUserFilteredQuestions);
 router.route('/usedtags').get(usedTags);
 router.route('/usedtags/:username').get(usedUserTags);
-router.route('/upvote/:id').put(authenticateToken,upvote);
-router.route('/downvote/:id').put(authenticateToken,downvote);
+router.route('/upvote/:id').put(upvote);
+router.route('/downvote/:id').put(downvote);
 router.route('/fetchVotes/:id').get(fetchVotes);
 router.route('/fetchallVotes').get(fetchAllVotes);
 router.route('/answeredQuestions').get(answeredQuestions);
